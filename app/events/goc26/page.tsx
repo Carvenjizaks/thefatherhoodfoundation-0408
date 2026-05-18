f6eefc9fdd615f89a8609deb2968317a93afa59a
Carven Izaks <carvenjizaks@gmail.com>
2026-05-18 13:57:39 +0000

    Trigger redeploy

--- app/events/goc26/page.tsx
+++ app/events/goc26/page.tsx
@@ -3,6 +3,7 @@ export default function GOC26Page() {
     <div style={{ padding: '50px', textAlign: 'center' }}>
       <h1>Gathering of Champions 2026</h1>
       <p>Test page - July 17-18, 2026</p>
+      <p>Registration is now open!</p>
     </div>
   )
 }
